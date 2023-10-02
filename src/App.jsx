fc9f122f54313538b8dee197ce4d2ac9e6e65c68
Shujaa Gideon <shujaagideon@gmail.com>
2023-10-02 11:45:01 +0300

    simple adjustments🪶

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable react/no-unknown-property */
+import { Canvas, useFrame } from '@react-three/fiber'
 import './App.css'
+import { OrbitControls } from '@react-three/drei'
+import { Suspense } from 'react'
+import * as THREE from 'three'
+import tex from './assets/sphereTex2.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Canvas
+            gl={{ alpha: true}}
+            camera={{ position: [0, 15, 20], fov: 32.5, near: 1, far: 100 }}
+            className='h-full w-full'
+        >
+            <ambientLight intensity={1.8} />
+            <directionalLight position={[0, 5, -4]} intensity={4} />
+            <AnimatedSphere/>
+            <Suspense fallback={null}>
+                <OrbitControls dampingFactor={0.2} enableZoom={false}/>
+            </Suspense>
+        </Canvas>
     </>
   )
 }
 
 export default App
+
+function AnimatedSphere() {
+  const texture = new THREE.TextureLoader().load(tex);
+  texture.colorSpace = THREE.SRGBColorSpace;
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set( 2, 2 );
+
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime()
+    // texture.offset.y += 0.15;
+    // texture.offset.y += 0.15;
+    texture.offset.y =  a * 0.25;
+})
+
+  return (
+    <mesh position={[0,-1,0]}>
+        <sphereGeometry args={[5, 50, 50]} />
+        <meshStandardMaterial map={texture} transparent />
+    </mesh>
+)
+}
